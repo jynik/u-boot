e89845609cd05599aad643a2aeaee30bb35995ec
Jon Szymaniak <jon.szymaniak.foss@gmail.com>
2020-12-26 19:58:22 -0600

    Current progress on porting imx6ull_som from Emcraft 2016.11 fork

---
+++ board/emcraft/imx6ull_som/imx6ull_som.c
@@ -0,0 +1,271 @@
+/*
+ * Copyright (C) 2017 Emcraft Systems
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <asm/arch/clock.h>
+#include <asm/arch/iomux.h>
+#include <asm/arch/imx-regs.h>
+#include <asm/arch/crm_regs.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/gpio.h>
+#include <asm/io.h>
+#include <common.h>
+#include <fsl_esdhc.h>
+#include <linux/sizes.h>
+#include <mmc.h>
+#include <phy.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/* MMDC P0/P1 Registers */
+struct mmdc_p_regs {
+	u32 mdctl;
+	u32 mdpdc;
+	u32 mdotc;
+	u32 mdcfg0;
+	u32 mdcfg1;
+	u32 mdcfg2;
+	u32 mdmisc;
+	u32 mdscr;
+	u32 mdref;
+	u32 res1[2];
+	u32 mdrwd;
+	u32 mdor;
+	u32 mdmrr;
+	u32 mdcfg3lp;
+	u32 mdmr4;
+	u32 mdasp;
+	u32 res2[239];
+	u32 maarcr;
+	u32 mapsr;
+	u32 maexidr0;
+	u32 maexidr1;
+	u32 madpcr0;
+	u32 madpcr1;
+	u32 madpsr0;
+	u32 madpsr1;
+	u32 madpsr2;
+	u32 madpsr3;
+	u32 madpsr4;
+	u32 madpsr5;
+	u32 masbs0;
+	u32 masbs1;
+	u32 res3[2];
+	u32 magenp;
+	u32 res4[239];
+	u32 mpzqhwctrl;
+	u32 mpzqswctrl;
+	u32 mpwlgcr;
+	u32 mpwldectrl0;
+	u32 mpwldectrl1;
+	u32 mpwldlst;
+	u32 mpodtctrl;
+	u32 mprddqby0dl;
+	u32 mprddqby1dl;
+	u32 mprddqby2dl;
+	u32 mprddqby3dl;
+	u32 mpwrdqby0dl;
+	u32 mpwrdqby1dl;
+	u32 mpwrdqby2dl;
+	u32 mpwrdqby3dl;
+	u32 mpdgctrl0;
+	u32 mpdgctrl1;
+	u32 mpdgdlst0;
+	u32 mprddlctl;
+	u32 mprddlst;
+	u32 mpwrdlctl;
+	u32 mpwrdlst;
+	u32 mpsdctrl;
+	u32 mpzqlp2ctl;
+	u32 mprddlhwctl;
+	u32 mpwrdlhwctl;
+	u32 mprddlhwst0;
+	u32 mprddlhwst1;
+	u32 mpwrdlhwst0;
+	u32 mpwrdlhwst1;
+	u32 mpwlhwerr;
+	u32 mpdghwst0;
+	u32 mpdghwst1;
+	u32 mpdghwst2;
+	u32 mpdghwst3;
+	u32 mppdcmpr1;
+	u32 mppdcmpr2;
+	u32 mpswdar0;
+	u32 mpswdrdr0;
+	u32 mpswdrdr1;
+	u32 mpswdrdr2;
+	u32 mpswdrdr3;
+	u32 mpswdrdr4;
+	u32 mpswdrdr5;
+	u32 mpswdrdr6;
+	u32 mpswdrdr7;
+	u32 mpmur0;
+	u32 mpwrcadl;
+	u32 mpdccr;
+};
+
+int dram_init(void)
+{
+	struct mmdc_p_regs *mmdc0 =
+		(struct mmdc_p_regs *)MMDC_P0_BASE_ADDR;
+	long mdctl;
+
+	/*
+	 * One of the following DDR devices can be installed:
+	 *   MT41K128M16JT-107:K (256 MB)
+	 *   MT41K256M16TW-107 IT:P (512 MB)
+	 */
+	gd->ram_size = get_ram_size((long *)PHYS_SDRAM, 512 * 1024 * 1024);
+
+	if (gd->ram_size == (256 * 1024 * 1024)) {
+		/*
+		 * Decrease Row to 14 bits.
+		 * We cannot place MMDC into configuration mode
+		 * since we are running from RAM.
+		 */
+		mdctl = readl(&mmdc0->mdctl);
+		mdctl &= 0xf8ffffff;
+		mdctl |= (14 - 11) << 24;
+		writel(mdctl, &mmdc0->mdctl);
+
+	} else if (gd->ram_size == (512 * 1024 * 1024)) {
+		/* Do nothing: already configured in imximage.cfg */
+
+	} else {
+		printf("Auto DDR size detection failed! Detected %li MiB\n",
+			gd->ram_size / 1024 / 1024);
+	}
+
+	return 0;
+}
+
+int board_mmc_get_env_dev(int devno)
+{
+	return devno;
+}
+
+int mmc_map_to_kernel_blk(int devno)
+{
+	return devno;
+}
+
+int board_early_init_f(void)
+{
+	struct mxc_ccm_reg *imx_ccm = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
+	u32 cacrr = __raw_readl(&imx_ccm->cacrr);
+	u32 pll1 = __raw_readl(&imx_ccm->analog_pll_sys);
+	/* PLL1 (ARM PLL) Fout = Fin * DIV_SELECT / 2, Fin = 24MHz */
+	u32 div = get_cpu_speed_grade_hz() * 2 / MXC_HCLK;
+
+	/* Set CACRR[ARM_PODF] divider to value 0: "divide by 1",
+	 * making ARM_CLK_ROOT equal to PLL1.
+	 */
+	cacrr &= ~(MXC_CCM_CACRR_ARM_PODF_MASK << MXC_CCM_CACRR_ARM_PODF_OFFSET);
+	__raw_writel(cacrr, &imx_ccm->cacrr);
+
+	pll1 &= ~BM_ANADIG_PLL_SYS_DIV_SELECT;
+	pll1 |= (div & BM_ANADIG_PLL_SYS_DIV_SELECT);
+	__raw_writel(pll1, &imx_ccm->analog_pll_sys);
+
+	timer_init();
+
+	// ENET1_RX1, aka GPIO2_IO01, aka PHYAD2
+	// Pull-down, open drain
+	writel(5, 0x20e00c8);
+	writel(0x18a0, 0x20e0354);
+	writel(0x2, 0x20a0004);
+	writel(0, 0x20a0000);
+
+	// GPIO5_IO04, aka 3V3_P+5V_P
+	writel(5, 0x2290018);
+	writel(0x10, 0x20AC004);
+	writel(0xb0a0, 0x229005C);
+
+	// GPIO3_IO04, aka nWDOG_OUT
+	writel(5, 0x20e0114);
+	writel(0x10, 0x20A4004);
+	writel(0xf8a0, 0x20E03A0);
+
+	// GPIO5_IO04, aka 3V3_P+5V_P -> off
+	writel(0, 0x20AC000);
+	// GPIO3_IO04, aka nWDOG_OUT -> ASSRT
+	writel(0, 0x20A4000);
+
+	mdelay(100);
+
+	// ENET1_RX1, aka GPIO2_IO01, aka PHYAD2
+	// Configure as Pull-up
+	writel(0x80a0, 0x20e0354);
+#if (CONFIG_FEC_ENET_DEV == 0)
+	writel((CONFIG_FEC_MXC_PHYADDR == 5) ? 0x2 : 0, 0x20a0000);
+#endif
+
+	// GPIO5_IO04, aka 3V3_P+5V_P -> on
+	writel(0x10, 0x20AC000);
+
+	// GPIO3_IO04, aka nWDOG_OUT -> OK
+	writel(0x10, 0x20A4000);
+
+	mdelay(50);
+
+	// ENET1_RX1, aka GPIO2_IO01, aka PHYAD2
+	writel(0, 0x20a0004);
+	writel(0, 0x20a0000);
+	writel(0, 0x20e00c8);
+	writel((3 << 6) | (1 << 13) | (1 << 12) | (3 << 14), 0x20e0354);
+
+	mdelay(50);
+
+	return 0;
+}
+
+static int board_init_fec(void)
+{
+#if defined(CONFIG_FEC_ENET_DEV)
+	struct iomuxc *const iomuxc_regs = (struct iomuxc *)IOMUXC_BASE_ADDR;
+	int ret = 0;
+
+	#if (CONFIG_FEC_ENET_DEV == 0)
+	clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC1_MASK, IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
+	ret = enable_fec_anatop_clock(CONFIG_FEC_ENET_DEV, ENET_50MHZ);
+	#else
+	clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC2_MASK, 0);
+	#endif
+
+	if (ret)
+		return ret;
+
+	enable_enet_clk(true);
+#endif /* CONFIG_FEC_ENET_DEV */
+
+	return 0;
+}
+
+int board_phy_config(struct phy_device *phydev)
+{
+	#if (CONFIG_FEC_ENET_DEV == 0)
+	phy_write(phydev, CONFIG_FEC_MXC_PHYADDR, 0x1f, 0x8180);
+	#endif
+
+	if (phydev->drv->config)
+		return phydev->drv->config(phydev);
+
+	return 0;
+}
+
+int board_init(void)
+{
+	/* Address of boot parameters */
+	gd->bd->bi_boot_params = PHYS_SDRAM + 0x100;
+
+	board_init_fec();
+
+	return 0;
+}
+
+u32 get_board_rev(void)
+{
+	return get_cpu_rev();
+}
---
+++ include/configs/imx6ull_som.h
@@ -0,0 +1,173 @@
+/*
+ * Copyright (C) 2017 Emcraft Systems
+ *
+ * Configuration settings for the Emcraft i.MX 6ULL System-On-Module.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+
+#ifndef __IMX6ULL_SOM_CONFIG_H
+#define __IMX6ULL_SOM_CONFIG_H
+
+// FIXME: Why doesn't the USB Armory use something similar here?
+#include "mx6_common.h"
+
+#ifdef CONFIG_SECURE_BOOT
+#ifndef CONFIG_CSF_SIZE
+#define CONFIG_CSF_SIZE			0x4000
+#endif
+#endif
+
+#define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+
+#define CONFIG_SYS_MALLOC_LEN		(16 * SZ_1M)
+
+#define CONFIG_MXC_UART
+#define CONFIG_MXC_UART_BASE		UART1_BASE
+
+#define CONFIG_SYS_FSL_ESDHC_ADDR	USDHC2_BASE_ADDR
+#define CONFIG_SYS_FSL_USDHC_NUM	1
+
+#define CONFIG_MII
+#define CONFIG_FEC_MXC
+#define CONFIG_FEC_ENET_DEV		0
+#define CONFIG_FEC_XCV_TYPE             RMII
+#define CONFIG_ETHPRIME                 "FEC"
+#if (CONFIG_FEC_ENET_DEV == 0)
+#define IMX_FEC_BASE			ENET_BASE_ADDR
+#define CONFIG_FEC_MXC_PHYADDR          5
+#elif (CONFIG_FEC_ENET_DEV == 1)
+#define IMX_FEC_BASE			ENET2_BASE_ADDR
+#define CONFIG_FEC_MXC_PHYADDR          1
+#endif /* CONFIG_FEC_ENET_DEV */
+#define CONFIG_PHYLIB
+#define CONFIG_PHY_MICREL
+
+#define CONFIG_BOOTCOMMAND \
+	   "run nandboot || echo 'Boot from NAND failed, run the update command'"
+
+#define SPLASH_FLASH_BASE		0x400000
+#define SPLASH_PART_SIZE		0xa00000
+
+#define DTB_PART_SIZE			0x20000
+#define KERNEL_PART_SIZE		0xc00000
+#define ROOTFS_PART_SIZE		0xa000000
+
+# define DTB_FLASH_BASE			0xe00000	/* (SPLASH_FLASH_BASE + SPLASH_PART_SIZE) */
+# define KERNEL_FLASH_BASE		0xe20000	/* (DTB_FLASH_BASE + DTB_PART_SIZE) */
+# define ROOTFS_FLASH_BASE		0x1a20000	/* (KERNEL_FLASH_BASE + KERNEL_PART_SIZE) */
+
+#define REV_EXTRA_ENV							\
+	"addip=setenv bootargs ${bootargs}"				\
+	" ip=${ipaddr}:${serverip}:${gatewayip}:${netmask}:${hostname}:eth0:off" \
+	" fec_mac=${ethaddr}\0"						\
+									\
+	"reliableboot=run nandboot\0"					\
+									\
+	"netboot=tftp ${loadaddr} ${image}"				\
+	" && tftp ${dtb_addr} ${dtbimage}"				\
+	" && run args addip && run boot_dtb\0"				\
+									\
+	"nandboot=nboot ${loadaddr} 0 ${kernel_offset}" \
+	" && nand read ${dtb_addr} ${dtb_offset} ${dtb_size}"		\
+	" && run args addip && run boot_dtb\0"				\
+									\
+	"args_common=console=ttymxc0,115200 consoleblank=0 panic=3\0"	\
+									\
+	"args_quiet=setenv bootargs ${args_common} ${ubirfs}"		\
+	" quiet=quiet\0"						\
+									\
+	"args_verbose=setenv bootargs ${args_common} ${ubirfs}"		\
+	" ignore_loglevel\0"						\
+									\
+	"fullupdate=run update && run dtbupdate"			\
+	" && run rootfsupdate\0"					\
+									\
+	"update=nand erase ${kernel_offset} ${kernel_size}"	\
+	" && tftp ${loadaddr} ${image} "				\
+	" && nand write ${loadaddr} ${kernel_offset} ${filesize}\0"	\
+									\
+	"dtbupdate=nand erase ${dtb_offset} ${dtb_size}"		\
+	" && tftp ${loadaddr} ${dtbimage} "				\
+	" && nand write ${loadaddr} ${dtb_offset} ${filesize}\0"	\
+									\
+	"rootfsupdate=nand erase ${rootfs_offset} ${rootfs_size}"	\
+	" && tftp ${loadaddr} ${rootfsimage} "				\
+	" && nand write ${loadaddr} ${rootfs_offset} ${filesize}\0"	\
+									\
+	"splashupdate=nand erase ${splash_offset} ${splash_size}"	\
+	" && tftp ${loadaddr} ${splashimage} "				\
+	" && nand write ${loadaddr} ${splash_offset} ${filesize}\0"	\
+									\
+	"boot_dtb=bootm ${loadaddr} - ${dtb_addr}\0"			\
+									\
+	"verify=yes\0"							\
+	"splash_offset="	__stringify(SPLASH_FLASH_BASE) "\0"	\
+	"dtb_offset="		__stringify(DTB_FLASH_BASE) "\0"	\
+	"kernel_offset="	__stringify(KERNEL_FLASH_BASE) "\0"	\
+	"rootfs_offset="	__stringify(ROOTFS_FLASH_BASE) "\0"	\
+	"splash_size="		__stringify(SPLASH_PART_SIZE) "\0" 	\
+	"dtb_size="		__stringify(DTB_PART_SIZE) "\0"		\
+	"kernel_size="		__stringify(KERNEL_PART_SIZE) "\0"	\
+	"rootfs_size="		__stringify(ROOTFS_PART_SIZE) "\0"	\
+
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+	"autoload=yes\0"						\
+	"ethaddr=3C:FB:96:08:08:29\0"					\
+	"ipaddr=172.17.77.14\0"						\
+	"serverip=172.17.0.19\0"					\
+	"netmask=255.255.0.0\0"						\
+	"hostname=6ull_1\0"						\
+	"image=imx/imx6ull-som/rootfs.uImage\0"				\
+	"dtbimage=imx/imx6ull-som/rootfs.dtb\0"				\
+	"rootfsimage=imx/imx6ull-som/rootfs.ubi\0"			\
+	"splashimage=imx/imx6ull-som/splash.rgb\0"			\
+	"args=run args_verbose\0"					\
+									\
+	"ubirfs=rootwait=1 rw ubi.mtd=4,2048 rootfstype=ubifs"		\
+	" root=ubi0:rootfs ubi.fm_autoconvert=1\0"			\
+									\
+	"args_nfs=setenv bootargs ${args_common} ignore_loglevel"	\
+	" earlyprintk root=/dev/nfs rw"					\
+	" nfsroot=172.17.0.1:/work/sergmir/imx-rootfs,v3,nolock\0"	\
+									\
+	"dtb_addr=0x83000000\0"						\
+	REV_EXTRA_ENV
+
+/* Miscellaneous configurable options */
+
+//#define CONFIG_SYS_MEMTEST_START	0x80000000
+//#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + 0x8000000)
+
+#define CONFIG_SYS_LOAD_ADDR		CONFIG_LOADADDR
+#define CONFIG_SYS_HZ			1000
+
+/* Physical Memory Map */
+#define CONFIG_NR_DRAM_BANKS		1
+#define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
+
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM
+#define CONFIG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CONFIG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#define CONFIG_SYS_INIT_SP_OFFSET \
+	(CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
+#define CONFIG_SYS_INIT_SP_ADDR \
+	(CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
+
+#define CONFIG_NAND_MXS
+#define CONFIG_SYS_MAX_NAND_DEVICE	1
+#define CONFIG_SYS_NAND_BASE		MXS_GPMI_BASE
+
+#define CONFIG_IMX_THERMAL
+
+#define CONFIG_IOMUX_LPSR
+
+#define CONFIG_SOFT_SPI
+
+#define CONFIG_APBH_DMA
+#define CONFIG_APBH_DMA_BURST
+#define CONFIG_APBH_DMA_BURST8
+
+#endif /* __IMX6ULL_SOM_CONFIG_H */
